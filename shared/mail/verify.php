57ce0e1fd06582566e1c8a4ed065d338544c1dd5
Paulos Teshome <paulman7792@gmail.com>
2022-06-07 19:10:52 +0300

    Update verify.php

--- shared/mail/verify.php
+++ shared/mail/verify.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>User Account Activation by Email Verification using PHP</title>
-<!-- CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
 <?php
 session_start();
 if($_GET['key'] && $_GET['token'])
@@ -53,15 +43,3 @@ else
     echo "Danger! Your something went wrong.";
 }
 ?>
-<div class="container mt-3">
-<div class="card">
-<div class="card-header text-center">
-User Account Activation by Email Verification using PHP
-</div>
-<div class="card-body">
-<p><?php echo $msg; ?></p>
-</div>
-</div>
-</div>
-</body>
-</html>
